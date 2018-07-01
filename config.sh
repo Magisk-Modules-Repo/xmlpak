4f53366fbed99fe00f5653a6de51d1b545667f18
Tadi <tadi@example.com>
2018-07-01 23:32:03 +0200

    Initial commit of xmlpak

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     xmlpak Magisk Module    "
   ui_print "*******************************"
 }
 
